ef93e9f5bc4c265f15c56fda09b0dca9737053d8
wisftock <fullstack.sw@gmail.com>
2022-08-10 11:42:46 -0500

    fead: add configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Route, Routes } from 'react-router-dom';
+
 const App = () => {
   return (
     <div>
-      <h1>Hello word!</h1>
+      <Routes>
+        <Route path='/' element={<h1>Hello work!</h1>} />
+      </Routes>
     </div>
   );
 };
